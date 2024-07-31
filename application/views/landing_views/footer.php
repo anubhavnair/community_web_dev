a41013125d2802d32a7648868bb0e4b79baea754
raja dewangan <kingsunknown01@gmail.com>
2024-07-31 13:40:42 +0530

    header footer

---
+++ application/views/landing_views/footer.php
@@ -0,0 +1,17 @@
+       
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/landing_assests/lib/wow/wow.min.js"></script>
+    <script src="assets/landing_assests/lib/easing/easing.min.js"></script>
+    <script src="assets/landing_assests/lib/waypoints/waypoints.min.js"></script>
+    <script src="assets/landing_assests/lib/counterup/counterup.min.js"></script>
+    <script src="assets/landing_assests/lib/lightbox/js/lightbox.min.js"></script>
+    <script src="assets/landing_assests/lib/owlcarousel/owl.carousel.min.js"></script>
+    
+
+    <!-- Template Javascript -->
+    <script src="assets/landing_assets/js/main.js"></script>
+    </body>
+
+</html>
