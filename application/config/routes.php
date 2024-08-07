0ef79bd24b2269f5d0804ed28e652d3905abdf13
Hemant Kumar <itshemant566@gmail.com>
2024-08-07 15:56:19 +0530

    changed Default Routes Added index func in welcome commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -49,14 +49,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'landing_page_controllers/LandingPagesController';
+$route['default_controller'] = 'welcome';
 // $route['404_override'] = '';
 // $route['translate_uri_dashes'] = FALSE;
 
 
 
 
-$route['home'] = 'landing_page_controllers/LandingPagesController';
+// $route['home'] = 'landing_page_controllers/LandingPagesController';
 
 //landing page
 
